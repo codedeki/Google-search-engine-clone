16ceca4bc3896adf6243aee70b9a652003880d09
codedeki <35740865+codedeki@users.noreply.github.com>
2019-11-14 10:34:34 -0500

    Add files via upload

---
+++ freegle/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Welcome to Freegle</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Search Engine, websites, images">
+    <meta name="keywords" content="Search engine, freegle, websites">
+    <meta name="author" content="Freegle Foundation">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+  </head>
+  <body>
+
+    <div class="wrapper indexPage">
+
+      <div class="mainSection">
+
+        <div class="logoContainer">
+          <img src= "assets/images/freegle1.png" title="Site logo" alt="Site logo">
+        </div>
+
+        <div class="searchContainer">
+
+          <form action="search.php" method="GET">
+
+
+            <input class="searchBox" type="text" name="term" placeholder="Enter a search term..."/>
+            <input class="searchButton" type="submit" value="Search"/>
+
+          </form>
+
+        </div>
+
+      </div>
+
+    </div>
+  </body>
+</html>
